97583857809f7128aa2832e61b49b1aeaf6e0674
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-04-12 13:36:32 -0400

    permisos y ejecucion de ficheros

--- 1_utilityPostgres.sh
+++ 1_utilityPostgres.sh
@@ -2,3 +2,16 @@
 # Programa para realizar operaciones de Postgres
 
 echo "Hola"
+echo "Primero 421: USUARIO, Segundo 421: GRUPO, Tercero 421: CUALQUIERUSUARIO: "
+echo "4: lectura r"
+echo "2: Escritura w"
+echo "1: Ejecución x"
+echo "Ejemplo: 764 "
+echo "7: Todos los permisos primer grupo"
+echo "6: Lectura, Escrituta"
+echo "4: Lectura"
+echo "rw - r - r --> 42 - 4 - 4 --> 644"
+echo "Solo ejecucion, con cambiar de modo = chmod +x"
+echo "ejecutar:"
+echo "bash ......"
+echo "./......"
