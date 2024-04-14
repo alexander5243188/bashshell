a2ad011914f5b1e204ea3f0f5bfabfa4a8fc6b2a
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-04-14 02:41:10 -0400

    declaracion de variables

---
+++ variables.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+echo "variables de entorno del sistema operativo"
+echo "/etc/profile"
+echo "COURSE_NAME=Programacion"
+echo "export COURSE_NAME"
+
+# Revisar las variables
+opcion=8:10
+nombre=Alexander
+
+echo "Hola $nombre llegaste a las $opcion"
